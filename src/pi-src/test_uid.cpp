93bc556b4b4903c11300a8631a2607b28cdb888a
Noah <noah@inspiredware.com>
2018-02-28 19:55:49 -0800

    See #14. Adds test file + updates makefile. Currently untested!
    Adds test cases that have not yet been compiled and executed

---
+++ src/pi-src/test_uid.cpp
@@ -0,0 +1,42 @@
+/******************************************************************************
+Test cases for UID decoding.
+
+Author:  Noah Strong
+Created: 2018-02-28
+******************************************************************************/
+
+#include "catch.hpp"
+
+#include "uid.h"
+#include "data_collection.h"
+
+TEST_CASE("UID decoding", "[uid_decoding]"){
+
+    SECTION("PING decoding"){
+        int id = 205;
+        ping a;
+        long duration = ((id * STEP_SIZE_MS) + MIN_PING_DUR_MS) * 1000;
+
+        a.pin = 0;
+        a.start = 0;
+        a.duration = duration;
+
+        REQUIRE(id_decode_ping(a) == id);
+    }
+
+    SECTION("DELAY decoding"){
+        int id = 320;
+        int dur = 10;
+        ping a, b;
+        long delay = ()(id * STEP_SIZE_MS) + MIN_DELAY_DUR_MS) * 1000;
+
+        a.pin = 0;
+        a.start = 0;
+        a.duration = dur;
+        b.pin = 0;
+        b.start = dur + delay;
+        b.duration = dur;
+
+        REQUIRE(id_decode_delay(a, b) == id);
+    }
+}
