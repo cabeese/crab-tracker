0f78a01da3f78528bb8e7c587cb0cdbf92e99cbd
cabeese <Noah.Strong@wwu.edu>
2018-03-26 11:54:20 -0700

    See #14. Adds documentation to header comments

--- src/pi-src/uid.h
+++ src/pi-src/uid.h
@@ -1,6 +1,17 @@
 /******************************************************************************
 Decodes unique identifiers from transmissions
 
+The details of UID encoding and decoding can be found in
+.../doc/TransmissionProtocol/TransmissionProtocol.pdf,
+also known as the iCRAB Protocol Documentation.
+
+Note that we expect some amount of noise or variance in our measured signals,
+so we can't rely on the measured durations or delays matching perfectly with
+our mathematical expectations. To compensate for this, we simply round each
+decoded ID (as a float) to the nearest integer. This way, if a given duration
+is 0.074ms longer than it "should be" based on our documentation, the small
+error is thrown away.
+
 Author:  Noah Strong
 Project: Crab Tracker
 Created: 2018-02-28
