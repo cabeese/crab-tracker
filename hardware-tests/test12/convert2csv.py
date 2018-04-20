76aafb9014335c6b623d403581e987bf791f5859
mimaxwell <mimaxwell7@gmail.com>
2018-04-20 16:13:54 -0700

    see #31. Gathering test data to investigate timing issues

---
+++ hardware-tests/test12/convert2csv.py
@@ -0,0 +1,13 @@
+# open the file with the time stamps
+# read from it
+# write the time stamps to a csv
+
+# do I want all the delta's associated with an i to be on the same line?
+# do I want i to be printed at the begining of the line?
+
+# possible formats:
+# i: t1,t2,t3,...
+# t1,t2,t3...
+ #in the second format you would know i by what line it's on
+
+ def main
